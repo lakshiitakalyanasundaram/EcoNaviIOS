21f5d02e8613ce8c4fa6f792d9e9a0d902b8775d
K Lakshiita <lakshiitakalyanasundaram@gmail.com>
2025-12-24 15:56:55 +0530

    Initial Commit

---
+++ econaviTests/econaviTests.swift
@@ -0,0 +1,17 @@
+//
+//  econaviTests.swift
+//  econaviTests
+//
+//  Created by lakshiita kalyanasundaram on 12/24/25.
+//
+
+import Testing
+@testable import econavi
+
+struct econaviTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
